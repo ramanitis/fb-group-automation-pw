e172b176e30e49b66f5276cd8897bf634d27003f
Raman Kaushik <136480512+ramanitis@users.noreply.github.com>
2025-12-05 13:02:09 +0530

    added server configuration to src

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-const main = () => {
-  console.log("Hello TypeScript Node.js!");
-};
+import 'dotenv/config';
+import http from "http";
+const port = process.env.PORT ?? 3000
 
-main();
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'content-type': 'application/json' });
+    res.end(JSON.stringify({ data: 'default route', method: req.method }))
+  } else {
+    res.writeHead(404, { 'content-type': 'application/json' });
+    res.end(JSON.stringify({ data: 'No other routes exist except default route' }))
+  }
+})
+
+server.listen(port, () => {
+  console.log(`server is listening at port : ${port}`);
+})
